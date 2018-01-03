f98cc3710e9086fe25b29a310e166911a3ac73d7
spzmd <b.keithdavid@gmail.com>
2018-01-03 14:04:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,15 +20,14 @@ function distanceTravelledInFeet(destination, beginning) {
 }
 
 function calculatesFarePrice(destination, beginning) {
-  if (((destination - beginning)*264) > 2000 && ((destination - beginning)*264) < 2500) {
-    return 25;
+  if (((destination - beginning)*264) <= 400) {
+    return 0;
   }
-  
   else if (((destination - beginning)*264)  <= 2000) {
     return 10.56;
   }
-  else if (((destination - beginning)*264) <= 400) {
-    return 0;
+  else if (((destination - beginning)*264) > 2000 && ((destination - beginning)*264) < 2500) {
+    return 25;
   }
   else
     return "cannot travel that far";
